6f5629415743473f65c4f57d63376ba64e062a64
Nik Revenco <pm@nikrev.com>
2026-03-29 09:19:16 +0000

    Hide lines with comments

--- src/lib.rs
+++ src/lib.rs
@@ -23,11 +23,11 @@
 //! Example:
 //!
 //! ```
-//! /*
+//! # /*
 //! tokio::select! {
 //!     Ok(res) = reader.read(&mut buf), if can_read => writer.write_all(res.bytes)
 //! }
-//! */
+//! # */
 //! ```
 //!
 //! `#[tokio_select]` applies to a `match` expression, which has a list of arms:
@@ -39,11 +39,11 @@
 //! Example:
 //!
 //! ```
-//! /*
+//! # /*
 //! match () {
 //!     Ok(res) | poll!(reader.read(&mut buf)) if can_read => writer.write_all(res.bytes)
 //! }
-//! */
+//! # */
 //! ```
 //!
 //! # Examples
@@ -53,7 +53,7 @@
 //! `tokio::select!`:
 //!
 //! ```
-//! /*
+//! # /*
 //! tokio::select! {
 //!     res = reader.read(&mut buf), if can_read => {
 //!         let n = res?;
@@ -65,13 +65,13 @@
 //!         return Ok(());
 //!     }
 //! }
-//! */
+//! # */
 //! ```
 //!
 //! `#[tokio_select]`:
 //!
 //! ```
-//! /*
+//! # /*
 //! #[tokio_select]
 //! match () {
 //!     Ok(n) | poll!(reader.read(&mut buf)) if can_read => {
@@ -81,7 +81,7 @@
 //!
 //!     _ | poll!(shutdown.recv()) => return Ok(()),
 //! }
-//! */
+//! # */
 //! ```
 //!
 //! Admittedly, the syntax is a little strange. But it's also formattable by `rustfmt`. Trade-offs, people, trade-offs!
@@ -89,7 +89,7 @@
 //! ## Rate-Limited Message Processor
 //!
 //! ```
-//! /*
+//! # /*
 //! tokio::select! {
 //!     biased;
 //!
@@ -102,13 +102,13 @@
 //!         tokio::time::sleep(Duration::from_millis(50)).await;
 //!     }
 //! }
-//! */
+//! # */
 //! ```
 //!
 //! `#[tokio_select]`:
 //!
 //! ```
-//! /*
+//! # /*
 //! #[tokio_select(biased)]
 //! match () {
 //!     Some(Message::Data { id, payload }) | poll!(rx.recv()) => {
@@ -120,7 +120,7 @@
 //!         tokio::time::sleep(Duration::from_millis(50)).await;
 //!     }
 //! }
-//! */
+//! # */
 //! ```
 //!
 //! # Requirements
@@ -148,11 +148,11 @@
 //! But if we want `rustfmt` to work, then the expression must parse as valid Rust syntax. A `match` expression is *almost* perfect for this:
 //!
 //! ```
-//! /*
+//! # /*
 //! match {
 //!     <pattern> (if <precondition>)? => <handler>,
 //! }
-//! */
+//! # */
 //! ```
 //!
 //! That covers:
@@ -166,11 +166,11 @@
 //! can expand to patterns, so we can abuse the fact that a match arm takes a `|`-separated list of "patterns":
 //!
 //! ```
-//! /*
+//! # /*
 //! match {
 //!     <pattern> | poll!(<future>) (if <precondition>)? => <handler>,
 //! }
-//! */
+//! # */
 //! ```
 //!
 //! And put whatever we need inside of the `poll!` "macro", which is really a "fake macro" that does nothing,
@@ -185,6 +185,15 @@
 //!
 //! ```txt
 //! <pattern> = <future> (e if <precondition>)? => <handler>,
+//! ```
+//!
+//! # Global import
+//!
+//! You can make the `tokio_select!` macro globally available in your crate, without needing to import it, with:
+//!
+//! ```
+//! #[macro_use(tokio_select)]
+//! extern crate better_tokio_select;
 //! ```
 #![allow(rustdoc::invalid_rust_codeblocks)]
 
