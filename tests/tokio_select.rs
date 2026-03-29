1950bfc17fa49e1bbc2fcf105d07aaa12ff7641f
Nik Revenco <pm@nikrev.com>
2026-03-29 09:34:31 +0000

    Add license notice to `tokio_select.rs` tests

--- tests/tokio_select.rs
+++ tests/tokio_select.rs
@@ -1,3 +1,32 @@
+//! These tests are adapted from the `tokio` project's `macro_select.rs` file, which tests
+//! `tokio::select!` macro. Every invocation of `tokio::select!` was replaced with `#[tokio_select]`.
+//!
+//! https://github.com/tokio-rs/tokio/blob/ee4de818065a97c0be0d6bfc18fb36725349aef8/tokio/tests/macros_select.rs
+//!
+//! License: MIT
+//!
+//! MIT License
+//!
+//! Copyright (c) Tokio Contributors
+//!
+//! Permission is hereby granted, free of charge, to any person obtaining a copy
+//! of this software and associated documentation files (the "Software"), to deal
+//! in the Software without restriction, including without limitation the rights
+//! to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//! copies of the Software, and to permit persons to whom the Software is
+//! furnished to do so, subject to the following conditions:
+//!
+//! The above copyright notice and this permission notice shall be included in all
+//! copies or substantial portions of the Software.
+//!
+//! THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//! IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//! FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//! AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//! LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//! OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//! SOFTWARE.
+
 #![allow(clippy::disallowed_names)]
 #![feature(proc_macro_hygiene, stmt_expr_attributes)]
 
